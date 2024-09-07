d6276912162ce06e71d9928f36fa1dd9c03604bd
Alidiamond <107695618+alidiamond1@users.noreply.github.com>
2024-09-07 11:19:45 +0300

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,6 +6,7 @@ const auth = require('../middleware/auth');
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/profile', auth, userController.getProfile);
+router.put('/profile', auth, userController.updateProfile);
 router.post('/forgot-password', userController.forgotPassword);
 router.post('/reset-password', userController.resetPassword);
 
