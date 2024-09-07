22c3dd7ea4858c2b4a7bd028cb756ca914f9c4e8
Alidiamond <107695618+alidiamond1@users.noreply.github.com>
2024-09-07 11:27:11 +0300

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 
 // User registration
@@ -103,6 +102,35 @@ exports.forgotPassword = async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
+  }
+};
+
+// Change password
+exports.changePassword = async (req, res) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ msg: 'User not found' });
+    }
+
+    // Check if the current password is correct
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Current password is incorrect' });
+    }
+
+    // Hash the new password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(newPassword, salt);
+
+    await user.save();
+
+    res.json({ msg: 'Password changed successfully' });
+  } catch (err) {
+    console.error('Error in changePassword:', err.message);
+    res.status(500).json({ msg: 'Server error', error: err.message });
   }
 };
 
